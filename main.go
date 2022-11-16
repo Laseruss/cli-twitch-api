acaa40d810fa505d3c1738ea9c1cdcecd753ff98
Laseruss <loden.micke@gmail.com>
2022-11-16 16:17:23 +0100

    added way to get top games streamed

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 		panic(err)
 	}
 
-	liveStreams, err := client.getLiveFollowing()
+	topGames, err := client.getTopGames()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", liveStreams.Data)
+	fmt.Printf("%+v\n", topGames.Data)
 }
 
 func (c *Client) getUserID(username string) error {
@@ -69,7 +69,11 @@ type Following struct {
 	} `json:"data"`
 }
 
-func (c *Client) getLiveFollowing() (*Following, error) {
+// Gets the currently live streams from the users follower list.
+// Needs a user access token to be able to use.
+// Also the user access token needs to relate to the user id.
+// (Can not look for another accounts live followers)
+func (c Client) getLiveFollowing() (*Following, error) {
 	url := fmt.Sprintf("%s/streams/followed?user_id=%s", baseUrl, c.credentials.userId)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -95,4 +99,40 @@ func (c *Client) getLiveFollowing() (*Following, error) {
 	}
 
 	return &followerList, nil
+}
+
+type TopGames struct {
+	Data []struct {
+		ID        string `json:"id"`
+		Name      string `json:"name"`
+		BoxArtUrl string `json:"box_art_url"`
+	} `json:"data"`
+}
+
+func (c Client) getTopGames() (*TopGames, error) {
+	url := fmt.Sprintf("%s/games/top", baseUrl)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header = http.Header{
+		"Authorization": {fmt.Sprintf("Bearer %s", c.credentials.accessToken)},
+		"Client-Id":     {c.credentials.clientId},
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	topGames := TopGames{}
+
+	err = json.NewDecoder(resp.Body).Decode(&topGames)
+	if err != nil {
+		return nil, err
+	}
+
+	return &topGames, nil
 }
